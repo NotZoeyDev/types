004ef69ddb6c22cceca8507bac8e241897d073b0
Swishilicous <52143580+Swishilicous@users.noreply.github.com>
2022-03-26 14:24:54 -0500

    more specific webpack types

--- modules/webpack.d.ts
+++ modules/webpack.d.ts
@@ -5,15 +5,30 @@ interface DefaultOptions {
   defaultExport?: boolean;
 }
 
-interface BulkOptions extends DefaultOptions {
-  wait?: boolean;
+interface WaitForOptions {
+  retries?: number;
+  all?: boolean;
+  forever?: boolean;
+  delay?: number;
 }
 
-interface ExtendedOptions extends BulkOptions {
-  bulk?: boolean;
+interface ConditionalDefault extends DefaultOptions {
+  wait?: false;
 }
 
-interface DisplayNameOptions extends ExtendedOptions {
+interface ConditionalWaitFor extends WaitForOptions {
+  wait: true;
+}
+
+interface ConditionalBulk extends ConditionalDefault {
+  bulk: true;
+}
+
+interface ConditionalWaitBulk extends ConditionalWaitFor {
+  bulk: true;
+}
+
+interface DisplayNameOptions {
   default?: boolean;
 }
 
@@ -73,8 +88,12 @@ declare module '@webpack' {
    * getByProps('prop1', 'prop2');
    * ```
    */
-  export function getByProps(...options: [...props: string[], options: ExtendedOptions]): any;
-  export function findByProps(...options: [...props: string[], options: ExtendedOptions]): any;
+  export function getByProps(...options: [...props: string[], options: ConditionalDefault]): any;
+
+  /**
+   * Alias for getByProps.
+   */
+  export function findByProps(...options: [...props: string[], options: ConditionalDefault]): any;
 
   /**
    * Takes a displayName argument and searches webpack cache for a direct match.
@@ -85,32 +104,32 @@ declare module '@webpack' {
    * getByDisplayName('Switch', { default: false });
    * ```
    */
-  export function getByDisplayName(displayName: string, options?: DisplayNameOptions): any;
+  export function getByDisplayName(displayName: string, options?: DisplayNameOptions & ConditionalDefault): any;
 
   /**
    * Alias for getByDisplayName.
    */
-  export function findByDisplayName(displayName: string, options?: DisplayNameOptions): any;
+  export function findByDisplayName(displayName: string, options?: DisplayNameOptions & ConditionalDefault): any;
 
   /**
    * Converts all "stringable" modules to a string and searches for an indirect match much like getByProps.
    */
-  export function getByString(...options: [...strings: string[], options: ExtendedOptions]): any;
+  export function getByString(...options: [...strings: string[], options: ConditionalDefault]): any;
 
   /**
    * Alias for getByString.
    */
-  export function findByString(...options: [...strings: string[], options: ExtendedOptions]): any;
+  export function findByString(...options: [...strings: string[], options: ConditionalDefault]): any;
 
   /**
    * Similar to getByString except it stringifys the default export.
    */
-  export function getByDefaultString(...options: [...strings: string[], options: ExtendedOptions]): any;
+  export function getByDefaultString(...options: [...strings: string[], options: ConditionalDefault]): any;
 
   /**
    * Alias for getByDefaultString.
    */
-  export function findByDefaultString(...options: [...strings: string[], options: ExtendedOptions]): any;
+  export function findByDefaultString(...options: [...strings: string[], options: ConditionalDefault]): any;
 
   /**
    * A quicker way to fetch multiple modules at a time.
@@ -130,7 +149,7 @@ declare module '@webpack' {
    * );
    * ```
    */
-  export function bulk(...options: [...filters: SearchFilter[], options: BulkOptions]): any;
+  export function bulk(...options: [...filters: SearchFilter[], options: ConditionalDefault]): any;
 
   /**
    * Only to be used as a developer tool for finding modules.
@@ -146,12 +165,42 @@ declare module '@webpack' {
 
   // Overloads
   export function bulk(...filters: SearchFilter[]): any;
+  export function bulk(...options: [...filters: SearchFilter[], options: ConditionalWaitFor]): any;
+
   export function getByProps(...props: string[]): any;
+  export function getByProps(...options: [...props: string[], options: ConditionalWaitFor]);
+  export function getByProps(...options: [...props: (string | string[])[], options: ConditionalBulk]);
+  export function getByProps(...options: [...props: (string | string[])[], options: ConditionalWaitBulk]);
+
   export function findByProps(...props: string[]): any;
+  export function findByProps(...options: [...props: string[], options: ConditionalWaitFor]): any;
+  export function findByProps(...options: [...props: (string | string[])[], options: ConditionalBulk]): any;
+  export function findByProps(...options: [...props: (string | string[])[], options: ConditionalWaitBulk]): any;
+
+  export function getByDisplayName(displayName: string): any;
+  export function getByDisplayName(displayName: string, options: DisplayNameOptions & ConditionalWaitFor): any;
+  export function getByDisplayName(...options: [...displayNames: string[], options: DisplayNameOptions & ConditionalBulk]): any;
+  export function getByDisplayName(...options: [...displayNames: string[], options: DisplayNameOptions & ConditionalWaitBulk]): any;
+
   export function getByString(...strings: string[]): any;
+  export function getByString(...options: [...props: string[], options: ConditionalWaitFor]): any;
+  export function getByString(...options: [...props: (string | string[])[], options: ConditionalBulk]): any;
+  export function getByString(...options: [...props: (string | string[])[], options: ConditionalWaitBulk]): any;
+
   export function findByString(...strings: string[]): any;
+  export function findByString(...options: [...props: string[], options: ConditionalWaitFor]): any;
+  export function findByString(...options: [...props: (string | string[])[], options: ConditionalBulk]): any;
+  export function findByString(...options: [...props: (string | string[])[], options: ConditionalWaitBulk]): any;
+
   export function getByDefaultString(...strings: string[]): any;
+  export function getByDefaultString(...options: [...props: string[], options: ConditionalWaitFor]): any;
+  export function getByDefaultString(...options: [...props: (string | string[])[], options: ConditionalBulk]): any;
+  export function getByDefaultString(...options: [...props: (string | string[])[], options: ConditionalWaitBulk]): any;
+
   export function findByDefaultString(...strings: string[]): any;
+  export function findByDefaultString(...options: [...props: string[], options: ConditionalWaitFor]): any;
+  export function findByDefaultString(...options: [...props: (string | string[])[], options: ConditionalBulk]): any;
+  export function findByDefaultString(...options: [...props: (string | string[])[], options: ConditionalWaitBulk]): any;
 
   /**
    * Quick filters for the "bulk" function.

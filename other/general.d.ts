7d39c5f345f12a11024bfce686293f3734552447
Swishilicous <52143580+Swishilicous@users.noreply.github.com>
2022-03-25 22:41:02 -0500

    change useless type (desc) callee isn't even usable in strict mode plus using IArguments breaks destructuring

--- other/general.d.ts
+++ other/general.d.ts
@@ -31,30 +31,29 @@ declare interface Settings {
  */
 declare type Class = new (...args: any[]) => any;
 
-
 /**
  * The overwrite function argument for the "before" patch. 
  * @param {any} context - The "this" argument from the scope of the original function.
- * @param {IArguments} arguments - The arguments passed to the function as an array.
+ * @param {any[]} arguments - The arguments passed to the function as an array.
  * @param {any} original - The original function that you're patching.
- * @returns {IArguments | void} The arguments that will be passed to the original function (the default is the original arguments).
+ * @returns {any[] | void} The arguments that will be passed to the original function (the default is the original arguments).
  */
-declare type BeforeOverwrite = (context?: any, arguments?: IArguments, original?: Function) => IArguments | void;
+declare type BeforeOverwrite = (context?: any, arguments?: any[], original?: Function) => any[] | void;
 
 /**
  * The overwrite function argument for the "instead" patch.
  * @param {any} context - The "this" argument from the scope of the original function.
- * @param {IArguments} arguments - The arguments passed to the function as an array.
+ * @param {any[]} arguments - The arguments passed to the function as an array.
  * @param {Function} original - The original function that you're patching.
  * @returns {any | void} The value that is returned when the function is ran.
  */
-declare type InsteadOverwrite = (context?: any, arguments?: IArguments, original?: Function) => any | void;
+declare type InsteadOverwrite = (context?: any, arguments?: any[], original?: Function) => any | void;
 
 /**
  * The overwrite function argument for the "after" patch.
  * @param {any} context - The "this" argument from the scope of the original function.
- * @param {IArguments} arguments - The arguments passed to the function as an array.
+ * @param {any[]} arguments - The arguments passed to the function as an array.
  * @param {any} result - The original function return value.
  * @returns {any | void} The value that is returned when the function is ran (the default is the original return value). Tip: If you're mutating the return value directly and it's an object you don't have to return it because objects are references in JS!
  */
-declare type AfterOverwrite = (context?: any, arguments?: IArguments, result?: any) => any | void;
+declare type AfterOverwrite = (context?: any, arguments?: any[], result?: any) => any | void;

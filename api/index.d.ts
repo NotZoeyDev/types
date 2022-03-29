b9f147b3f2a5539aefa1dee711e2ea6a18dc2b11
Swishilicous <52143580+Swishilicous@users.noreply.github.com>
2022-03-28 22:02:02 -0500

    delete announcements

--- api/index.d.ts
+++ api/index.d.ts
@@ -3,5 +3,4 @@ declare module '@api' {
   export { default as toasts } from '@api/toasts';
   export { default as commands } from '@api/commands';
   export { default as settings } from '@api/settings';
-  export { default as announcements } from '@api/announcements';
 }

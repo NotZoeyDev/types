76df3cc7fa5238a5d7b0a40a194575287981c895
bot slave <41898282+github-actions[bot]@users.noreply.github.com>
2022-03-31 15:21:29 +0000

    build types

--- build/index.d.ts
+++ build/index.d.ts
@@ -1322,12 +1322,12 @@ declare module '@utilities/getReactInstance' {
    * @name getReactInstance
    * @description Gets a react instance from an HTML element
    * @param {HTMLElement} element - HTML element to get the react instance from
-   * @param {function} filter - The filter to run on the tree passed as the first argument
    * @return {any} Returns null if no prop is found or the prop if it's found.
    */
 
   export default function (element: HTMLElement): any;
 }
+
 
 declare module '@utilities' {
   export * as dom from '@utilities/dom';

12054460738620c7814728264174de2c5cbfbee6
Swishilicous <paige.git@gmail.com>
2022-03-31 10:33:08 -0500

    mimic recent jsdoc fixes bttw eternal u need to add back the second param in getNestedProp my bad lol

--- utilities/getNestedProp.d.ts
+++ utilities/getNestedProp.d.ts
@@ -3,9 +3,9 @@ declare module '@utilities/getNestedProp' {
    * @name getNestedProp
    * @description Gets a nested prop from an object safely by returning null if nothing is found
    * @param {object} object - The object to get the nested prop from
-   * @param {string} prop - The filter to run on the tree passed as the first argument
+   * @param {string} path - The path to the nested prop
    * @return {any} Returns null if no prop is found or the prop if it's found.
    */
 
-  export default function (object: object, prop: string): any;
+  export default function (object: object, path: string): any;
 }
